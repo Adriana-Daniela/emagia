2f7d09bc94eee7448ce89ff43fa99e7c5cc5e6a3
Adriana <adriana.capata@gmail.com>
2023-09-08 17:27:12 +0300

    minor naming changes

--- tests/Application/PlayerSelectorTest.php
+++ tests/Application/PlayerSelectorTest.php
@@ -11,13 +11,13 @@ use PHPUnit\Framework\TestCase;
 
 class PlayerSelectorTest extends TestCase
 {
-    #[DataProvider('playersProvider')]
+    #[DataProvider('playersDataProvider')]
     public function testCanGetFirstAttacker(
+        bool $expectedFirstAttacker,
         int $beastSpeed,
         int $heroSpeed,
         float $beastLuck,
-        float $heroLuck,
-        bool $isBeastFirstAttacker
+        float $heroLuck
     ): void
     {
         $playerSelector = new PlayerSelector();
@@ -28,51 +28,51 @@ class PlayerSelectorTest extends TestCase
         $beast->setSpeed($beastSpeed)->setLuck($beastLuck);
         $hero->setSpeed($heroSpeed)->setLuck($heroLuck);
 
-        $firstAttacker = $playerSelector->getFirstAttacker($beast, $hero);
+        $actualFirstAttacker = $playerSelector->getFirstAttacker($beast, $hero);
 
-        $this->assertSame($isBeastFirstAttacker, $firstAttacker);
+        $this->assertSame($expectedFirstAttacker, $actualFirstAttacker);
     }
 
     /**
      * @return array
      */
-    public static function playersProvider(): array
+    public static function playersDataProvider(): array
     {
         return [
             'beast is faster, beast strikes first' => [
+                'expectedFirstAttacker' => true,
                 'beastSpeed' => 57,
                 'heroSpeed' => 42,
                 'beastLuck' => 0.25,
                 'heroLuck' => 0.3,
-                'isBeastFirstAttacker' => true,
             ],
             'hero is faster, hero strikes first' => [
+                'isBeastFirstAttacker' => false,
                 'beastSpeed' => 42,
                 'heroSpeed' => 49,
                 'beastLuck' => 0.25,
                 'heroLuck' => 0.3,
-                'isBeastFirstAttacker' => false,
             ],
             'same speed but lucky beast, beast strikes first' => [
+                'isBeastFirstAttacker' => true,
                 'beastSpeed' => 45,
                 'heroSpeed' => 45,
                 'beastLuck' => 0.4,
                 'heroLuck' => 0.1,
-                'isBeastFirstAttacker' => true,
             ],
             'same speed but lucky hero, hero strikes first' => [
+                'isBeastFirstAttacker' => false,
                 'beastSpeed' => 45,
                 'heroSpeed' => 45,
                 'beastLuck' => 0.25,
                 'heroLuck' => 0.3,
-                'isBeastFirstAttacker' => false,
             ],
             'same speed, same luck, hero strikes first' => [
+                'isBeastFirstAttacker' => false,
                 'beastSpeed' => 45,
                 'heroSpeed' => 45,
                 'beastLuck' => 0.25,
                 'heroLuck' => 0.25,
-                'isBeastFirstAttacker' => false,
             ],
         ];
     }
